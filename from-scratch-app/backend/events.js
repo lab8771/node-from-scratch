e91f1fc1f4ae6354dc92d0ac86b600cb9c223d44
lab8771 <37773752+lab8771@users.noreply.github.com>
2020-10-16 11:23:12 -0400

    initial commit

---
+++ from-scratch-app/backend/events.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      id: 1,
+      title: 'Desjardins',
+      detail: 'Entrevue avec Desjardins',
+      date: '2020-09-02'
+    },
+   
+    {
+      id: 2,
+      title: 'Souper avec Marie-Claire',
+      date: '2020-09-01'
+    }
+  ];
